acd63368743a25ed669f261954feb48a480a166d
Jordan <jordanduzabonfim@gmail.com>
2026-06-23 14:23:29 -0300

    fix:transparent color converter

--- src/ImageConverter/converter.py
+++ src/ImageConverter/converter.py
@@ -14,14 +14,17 @@ with open(f"{file_name}.asm", "w") as f:
         for x in range(width):
             r, g, b, a = img.getpixel((x, y))
 
-            argb = (
-                (a << 24) |
-                (r << 16) |
-                (g << 8)  |
-                b
-            )
+            if a == 0:
+                pixels.append("0x00000000")
+            else:
+                argb = (
+                    (a << 24) |
+                    (r << 16) |
+                    (g << 8)  |
+                    b
+                )
 
-            pixels.append(f"0x{argb:08X}")
+                pixels.append(f"0x{argb:08X}")
 
     f.write("background:\n")
     f.write(".array ")
